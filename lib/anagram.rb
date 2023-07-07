be08cddab265b545e913f3d46781a35581f42546
Njerimacharia98 <njerimacharia05@gmail.com>
2023-07-07 14:32:20 +0300

    completed lab

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+  
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(words)
+      words.select { |w| anagram?(w) }
+    end
+  
+    private
+  
+    def anagram?(other_word)
+      sort_chars(word) == sort_chars(other_word) && word.downcase != other_word.downcase
+    end
+  
+    def sort_chars(word)
+      word.downcase.chars.sort.join
+    end
+  end
+  
